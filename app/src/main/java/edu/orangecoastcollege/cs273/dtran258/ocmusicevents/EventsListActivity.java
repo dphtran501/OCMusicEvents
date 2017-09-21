e6e44b717a1a3454f448c8a645f11b0e8e3d6afb
Derek <dphtran501@yahoo.com>
2017-09-21 14:33:10 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/ocmusicevents/EventsListActivity.java
@@ -0,0 +1,15 @@
+package edu.orangecoastcollege.cs273.dtran258.ocmusicevents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_list);
+    }
+}
