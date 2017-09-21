7f8454103c96055536fac2e6200186ed93a84ec7
Derek <dphtran501@yahoo.com>
2017-09-21 14:36:06 -0700

    Added MusicEvent.java and updated colors.xml

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/ocmusicevents/MusicEvent.java
@@ -0,0 +1,66 @@
+package edu.orangecoastcollege.cs273.dtran258.ocmusicevents;
+
+/**
+ * The MusicEvent class is a place holder for data corresponding to music events
+ * occurring within Orange County.  Currently, two arrays contain the data: one array
+ * stores the titles of each upcoming music event, while the other maintains the details
+ * about the event.
+ *
+ * @author Michael Paulding
+ * @version 1.0
+ */
+public class MusicEvent {
+
+    /**
+     * The titles of upcoming music events.
+     */
+    public static String[] titles = {
+            "Blink 182", "Ben Folds And A Piano", "Tony Bennett", "How The West Was Won",
+            "Will Downing", "Simple Plan", "Gwen Stefani", "Sublime With Rome"};
+
+    /**
+     * The details of upcoming music events.
+     */
+    public static String[] details = {
+            "October 7\n" +
+                    "Friday - 7:00 PM\n" +
+                    "Irvine Meadows (formerly Verizon Wireless)\n" +
+                    "8808 Irvine Center Drive\n" +
+                    "Irvine, California 92618",
+            "October 20\n" +
+                    "Thursday - 8:00 PM\n" +
+                    "City National Grove of Anaheim\n" +
+                    "2200 E Katella Ave\n" +
+                    "Anaheim, California" ,
+            "October 23\n" +
+                    "Sunday - 7:00 PM\n" +
+                    "Renée and Henry Segerstrom Concert Hall\n" +
+                    "615 Town Center Drive\n" +
+                    "Costa Mesa, California 92626" ,
+            "October 14\n" +
+                    "Friday - 6:30 PM\n" +
+                    "Irvine Meadows (formerly Verizon Wireless)\n" +
+                    "8808 Irvine Center Drive\n" +
+                    "Irvine, California 92618",
+            "September 30\n" +
+                    "Friday - 6:00 PM\n" +
+                    "Hyatt Newport Beach\n" +
+                    "1107 Jamboree Rd\n" +
+                    "Newport Beach, California 92660" ,
+            "October 4\n" +
+                    "Tuesday - 7:00 PM\n" +
+                    "The Observatory\n" +
+                    "3503 South Harbor Boulevard\n" +
+                    "Santa Ana, California 92704" ,
+            "October 29\n" +
+                    "Saturday - 7:30 PM\n" +
+                    "Irvine Meadows (formerly Verizon Wireless)\n" +
+                    "8808 Irvine Center Drive\n" +
+                    "Irvine, California 92618" ,
+            "October 15\n" +
+                    "Saturday   6:30 PM\n" +
+                    "Irvine Meadows (formerly Verizon Wireless)\n" +
+                    "8808 Irvine Center Drive\n" +
+                    "Irvine, California 92618"
+    };
+}
