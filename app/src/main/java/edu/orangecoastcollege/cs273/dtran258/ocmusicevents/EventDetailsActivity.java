f5afc2e7676ae33f789a72ed01de1c77196966b5
Derek <dphtran501@yahoo.com>
2017-09-21 15:50:14 -0700

    Added asset pictures, added round launcher icon, and finished EventDetailsActivity.java

--- app/src/main/java/edu/orangecoastcollege/cs273/dtran258/ocmusicevents/EventDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/ocmusicevents/EventDetailsActivity.java
@@ -1,10 +1,17 @@
 package edu.orangecoastcollege.cs273.dtran258.ocmusicevents;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class EventDetailsActivity extends AppCompatActivity
 {
@@ -20,11 +27,29 @@ public class EventDetailsActivity extends AppCompatActivity
         String title = intent.getStringExtra("title");
         String details = intent.getStringExtra("details");
 
+        // Create references to the text views
         TextView titleTextView = (TextView) findViewById(R.id.eventTitleTextView);
         TextView detailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+        ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
 
+        // Set the text of the text views
         titleTextView.setText(title);
         detailsTextView.setText(details);
+
+        // Use the Asset Manager to retrieve a file (image)
+        AssetManager am = getAssets();
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+        // Use AssetManager to open a stream to the file name
+        try
+        {
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        }
+        catch (IOException e)
+        {
+            Log.e("OC Music Events", "Error loading image: " + imageFileName);
+        }
 
     }
 
